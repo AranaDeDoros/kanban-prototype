fa4933c882395d6cd336e1230137150087353409
David Flores <2879168-devfloresitver@users.noreply.gitlab.com>
2025-11-10 00:10:36 -0600

    added incerceptors

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,59 @@
+// src/api/client.js
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "http://localhost:8000/api/",
 });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+// auto refresh
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // Si el token expiró y no hemos reintentado todavía
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      const refresh = localStorage.getItem("refresh");
+      if (!refresh) {
+        console.warn("No refresh token found, redirecting to login");
+        window.location.href = "/";
+        return Promise.reject(error);
+      }
+
+      try {
+        const res = await axios.post(
+          "http://localhost:8000/api/auth/token/refresh/",
+          {
+            refresh,
+          }
+        );
+
+        const newAccess = res.data.access;
+        localStorage.setItem("access", newAccess);
+        api.defaults.headers.Authorization = `Bearer ${newAccess}`;
+        originalRequest.headers.Authorization = `Bearer ${newAccess}`;
+
+        // Reintenta la petición original
+        return api(originalRequest);
+      } catch (refreshError) {
+        console.error("Refresh token failed", refreshError);
+        localStorage.removeItem("access");
+        localStorage.removeItem("refresh");
+        window.location.href = "/";
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
